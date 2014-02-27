a3d89ba14d6ce677b457b9395b2143d46cbbc26d
Evgeny Krutko <e.s.krutko@gmail.com>
2014-02-27 06:34:18 +0400

    init for CGMwP

---
+++ solvers/CGMwP/ConjGradMethWithPrec/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include "sbfMesh.h"
+
+/*!
+ * \brief main
+ * This program solves ordinary stress strain FEA problems (no decomposition)
+ *
+ */
+
+int main()
+{
+    std::string catalog("./");
+    std::string indFileName("ind.sba");
+    std::string crdFileName("crd.sba");
+    std::string mtrFileName("mtr001.sba");
+    std::string fixFileName("fix.sba");
+    std::string forceFileName("force.sba");
+
+   // std::unique_ptr<sbfMesh> =
+
+    return 0;
+}
+
