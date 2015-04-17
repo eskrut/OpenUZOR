89775110ea84bd6e3c57f70095ddaf1fc0b3b0a3
Evgeny Krutko <e.s.krutko@gmail.com>
2015-04-17 10:59:56 +0300

    Update ts file in qPrepareSE

--- utilities/qPrepareSE/ru_ru.ts
+++ utilities/qPrepareSE/ru_ru.ts
@@ -7,74 +7,72 @@
         <location filename="qpreparese.cpp" line="21"/>
         <location filename="qpreparese.cpp" line="68"/>
         <source>CHOUSE_IND_FILE</source>
-        <translation>Выбрать ind файл</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="22"/>
         <source>CHOUSE_IND_FILE_TOOLTIP</source>
-        <translation>Выбор файла индексов модели для проведения разбивки на суперэлементы</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="28"/>
         <source>USE_SEED</source>
-        <translation>Случайность</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="29"/>
         <source>USE_SEED_TOOLTIP</source>
-        <translation>Инициализировать алгоритм разбивки случайной величиной</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="30"/>
         <source>USE_INVERSE</source>
-        <translation>Обратный алгоритм</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="31"/>
         <source>USE_INVERSE_TOOLTIP</source>
-        <translation>Прямой алгоритм подразумевает следующее разбиение. Вся модель разбивается на нужное количество СЭ нижнего уровня, получившееся количество СЭ разбивается на второй уровень и т.д.
-Обратеый алгоритм реализуется следующим образом. Вся модель разбивается на необходимое количество СЭ верхнего уровня. Количество СЭ предыдущего уровня распределяется по возможности равномерно между СЭ верхнего уровня, происходит разбивка каждого СЭ верхнего уровня и т.д.
-Обратный алгоритм следует применять, если прямой алгоритм не может объеденить СЭ в следующем уровне подвое.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="32"/>
         <source>PARTITION_TOOLTIP</source>
-        <translation>Количество разбиений по уровням задается числами, разделенными запятой (например &quot;64,16,4&quot;). Числа идут от низшего уровня к высшему.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="33"/>
         <source>IMBALANCE_TOOLTIP</source>
-        <translation>Максимальный дисбаланс по уровням задается числами, разделенными запятой (например &quot;1.6,1.2&quot;). Возможно указать только одно число, которое будет применено ко всем уровням. Если уровней разбиения больше, чем указано значений дисбаланса, используется последнее значение дисбаланса.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="34"/>
         <source>LEVELBASE_TOOLTIP</source>
-        <translation>Базовая часть имени файлов уровней СЭ. К базовой части будет прибавлено &quot;00x.sba&quot;.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="35"/>
         <source>NODECONNECTION</source>
-        <translation>Связь по узлам</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="36"/>
         <source>NODECONNECTION_TOOLTIP</source>
-        <translation>Определение смежных узлов выполняется либо по определению общих граней, либо по определению общих узлов.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="39"/>
         <source>PARTITION</source>
-        <translation>Разбиение</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="41"/>
         <source>IMBALANCE</source>
-        <translation>Дисбаланс</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="43"/>
         <source>LEVELBASE</source>
-        <translation>База имени уровней</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="52"/>
@@ -82,22 +80,22 @@
         <location filename="qpreparese.cpp" line="142"/>
         <location filename="qpreparese.cpp" line="151"/>
         <source>MAKE</source>
-        <translation>Разбить</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="121"/>
         <source>PROCESSING</source>
-        <translation>Выполнение</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="128"/>
         <source>PART_FAILED_WITH_ERROR</source>
-        <translation>Разбиение прервано с ошибкой:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="qpreparese.cpp" line="138"/>
         <source>PART_FAILED_WITH_UNKNOWN_ERROR</source>
-        <translation>Разбиение прервано с неизвестной ошибкой.</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
