5554a151ad1f8346762f3b146aaa25ee10b186c4
Evgeny Krutko <e.s.krutko@gmail.com>
2015-05-08 08:43:08 +0300

    update reordered code

--- utilities/reorder/main.cpp
+++ utilities/reorder/main.cpp
@@ -1,3 +1,41 @@
-int main(int argc, char **argv) {
+#include <boost/program_options.hpp>
+#include <iostream>
+#include "reorderer.h"
+#include "sbfMesh.h"
+
+namespace po = boost::program_options;
+
+int main ( int argc, char **argv )
+{
+    std::string catalog, indName;
+    po::options_description desc ( "Program options" );
+    bool verbouse = false;
+    desc.add_options()
+    ( "help,h", "print help message" )
+    ( "work-dir,w", po::value<std::string> ( &catalog )->default_value ( "" ), "work catalog" )
+    ( "ind-file,i", po::value<std::string> ( &indName )->default_value ( "ind.sba" ), "ind file" )
+    ( "verbouse,v", "make verbouse output" )
+    ;
+    po::variables_map vm;
+    po::store ( po::parse_command_line ( argc, argv, desc ), vm );
+    po::notify ( vm );
+
+    if ( vm.count ( "help" ) || vm.count ( "h" ) ) { std::cout << desc << "\n"; return 1; }
+
+    if ( vm.count ( "verbouse" ) || vm.count ( "v" ) ) verbouse = true;
+
+    if ( catalog.size() && ( catalog.back() != '/' || catalog.back() != '\\' ) ) catalog += "/";
+
+    std::stringstream iName;
+    iName << catalog << indName;
+    //Creating mesh
+    std::unique_ptr<sbfMesh> pMesh ( std::make_unique<sbfMesh>() );
+    if ( pMesh->readIndFromFile ( iName.str().c_str() ) )
+    {std::cout << "error while mesh reading" << std::endl; return 1;}
+
+    Reorderer rord;
+
+    rord.renumber ( pMesh.get() );
+
     return 0;
 }
--- utilities/reorder/reorderer.cpp
+++ utilities/reorder/reorderer.cpp
@@ -1,6 +1,7 @@
 #include "reorderer.h"
 
 #include "sbfMesh.h"
+#include "sbfElement.h"
 #include "sbfReporter.h"
 
 #include <set>
@@ -17,46 +18,48 @@ Reorderer::~Reorderer()
 
 }
 
-std::pair<std::vector<int>, std::vector<int> > Reorderer::renumber(const std::vector<int> &rowShifts,
-                                                                   const std::vector<int> &columnIDs,
-                                                                   bool makeReport)
+std::pair<std::vector<int>, std::vector<int> > Reorderer::renumber ( const std::vector<int> &rowShifts,
+                                                                     const std::vector<int> &columnIDs,
+                                                                     bool makeReport )
 {
     std::pair<std::vector<int>, std::vector<int> > reorderIndexes;
-    reorderIndexes.first.resize(rowShifts.size()-1, -1);
-    reorderIndexes.second.resize(rowShifts.size()-1, -1);
+    reorderIndexes.first.resize ( rowShifts.size() - 1, -1 );
+    reorderIndexes.second.resize ( rowShifts.size() - 1, -1 );
 
     //METIS related code
     //TODO try parallel version
-    idx_t numNodes = rowShifts.size()-1;
+    idx_t numNodes = rowShifts.size() - 1;
     std::vector<idx_t> xadj;
     std::vector<idx_t> adjncy;
     std::vector<idx_t> perm;
     std::vector<idx_t> iperm;
-    xadj.reserve(rowShifts.size());
-    adjncy.reserve(columnIDs.size());
-    perm.resize(rowShifts.size()-1, -1);
-    iperm.resize(rowShifts.size()-1, -1);
-    for(auto p : rowShifts) xadj.push_back(p);
-    for(auto p : columnIDs) adjncy.push_back(p);
+    xadj.reserve ( rowShifts.size() );
+    adjncy.reserve ( columnIDs.size() );
+    perm.resize ( rowShifts.size() - 1, -1 );
+    iperm.resize ( rowShifts.size() - 1, -1 );
+    for ( auto p : rowShifts ) xadj.push_back ( p );
+    for ( auto p : columnIDs ) adjncy.push_back ( p );
     idx_t options[METIS_NOPTIONS];
-    METIS_SetDefaultOptions(options);
+    METIS_SetDefaultOptions ( options );
 
-    auto getBandwidth = [&](){
+    auto getBandwidth = [&] ( const std::vector<idx_t> &xadj, const std::vector<idx_t> &adjncy ) {
         size_t bandwidth = 0;
-        for(idx_t ct = 0; ct < numNodes; ++ct){
-            bandwidth += std::max(ct, adjncy[xadj[ct]]) - std::min(ct, adjncy[xadj[ct+1]-1]) + 1;
+        idx_t numNodes = xadj.size() - 1;
+        for ( idx_t ct = 0; ct < numNodes; ++ct ) {
+            bandwidth += std::max ( ct, adjncy[xadj[ct + 1] - 1] ) - std::min ( ct, adjncy[xadj[ct]] ) + 1;
         }
         return bandwidth;
     };
 
-    size_t bandwidth0 = getBandwidth();
+    size_t bandwidth0 = getBandwidth ( xadj, adjncy );
+    if ( makeReport ) report ( "Before reordering full bandwidth is", bandwidth0 );
 
     //TODO add options modification
-    int rez = METIS_NodeND(&numNodes, xadj.data(), adjncy.data(), nullptr, options, perm.data(), iperm.data());
+    int rez = METIS_NodeND ( &numNodes, xadj.data(), adjncy.data(), nullptr, options, perm.data(), iperm.data() );
 
-    if(rez != METIS_OK) {
+    if ( rez != METIS_OK ) {
         std::string err;
-        switch (rez) {
+        switch ( rez ) {
         case METIS_ERROR_INPUT:
             err = "METIS error input";
             break;
@@ -70,28 +73,63 @@ std::pair<std::vector<int>, std::vector<int> > Reorderer::renumber(const std::ve
             err = "Some unregistered error from METIS";
             break;
         }
-        report.error(err);
-        throw std::runtime_error(err);
+        report.error ( err );
+        throw std::runtime_error ( err );
     }
 
-    for(size_t ct = 0; ct < numNodes; ++ct){
+    size_t newShift = 0;
+    std::vector<idx_t> newXadj;
+    std::vector<idx_t> newAdjncy;
+    newXadj.reserve ( xadj.size() );
+    newAdjncy.reserve ( adjncy.size() );
+    newXadj.push_back ( newShift );
+    for ( idx_t ct = 0; ct < numNodes; ++ct ) {
+        int nodeToProcess = perm[ct];
         std::set<int> ids;
-        for(int shift = xadj[ct]; shift < xadj[ct+1]; ++shift)
-            ids.insert(iperm[adjncy[shift]]);
-        int shift = xadj[ct];
-        for(auto i : ids)
-            adjncy[shift++] = i;
+        for ( int shift = xadj[nodeToProcess]; shift < xadj[nodeToProcess + 1]; ++shift )
+            ids.insert ( iperm[adjncy[shift]] );
+        for ( auto i : ids )
+            newAdjncy[newShift++] = i;
+        newXadj.push_back ( newShift );
     }
 
-    size_t bandwidth1 = getBandwidth();
+    size_t bandwidth1 = getBandwidth ( newXadj, newAdjncy );
+    if ( makeReport ) report ( "After reordering full bandwidth is", bandwidth1,
+                                   "\nCompression is", static_cast<float> ( bandwidth0 ) / bandwidth1 );
 
     return reorderIndexes;
 }
 
-std::pair<std::vector<int>, std::vector<int> > Reorderer::renumber(const sbfMesh *mesh, bool makeReport)
+std::pair<std::vector<int>, std::vector<int> > Reorderer::renumber ( const sbfMesh *mesh, bool makeReport )
 {
-    std::pair<std::vector<int>, std::vector<int> > reorderIndexes;
+    std::vector<std::set<int>> connections;
+    int maxNodeID = 0;
+    mesh->applyToAllElements ( [&] ( const sbfElement & elem ) {
+        auto indexes = elem.indexes();
+        for ( size_t ct1 = 0; ct1 < indexes.size(); ++ct1 )
+            maxNodeID = std::max ( maxNodeID, indexes[ct1] );
+    } );
+    connections.resize ( maxNodeID + 1 );
+    mesh->applyToAllElements ( [&] ( const sbfElement & elem ) {
+        auto indexes = elem.indexes();
+        for ( size_t ct1 = 0; ct1 < indexes.size(); ++ct1 ) for ( size_t ct2 = 0; ct2 < indexes.size(); ++ct2 )
+                if ( ct1 != ct2 )
+                    connections[indexes[ct1]].insert ( indexes[ct2] );
+    } );
+    std::vector<int> rowShifts;
+    std::vector<int> columnIDs;
+    rowShifts.reserve ( mesh->numNodes() + 1 );
+    size_t colLength = 0;
+    for ( const auto &s : connections ) colLength += s.size();
+    columnIDs.reserve ( colLength );
+    colLength = 0;
+    rowShifts.push_back ( colLength );
+    for ( const auto &s : connections ) {
+        columnIDs.insert ( columnIDs.end(), s.begin(), s.end() );
+        colLength += s.size();
+        rowShifts.push_back ( colLength );
+    }
 
-    return reorderIndexes;
+    return renumber ( rowShifts, columnIDs, makeReport );
 }
 
